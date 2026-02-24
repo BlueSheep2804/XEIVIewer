0b3d2f329fa8e11c46fb7d99b707e7d187193d93
BlueSheep <48321445+BlueSheep2804@users.noreply.github.com>
2026-02-24 21:30:10 +0900

    feat: 🎸 完成品スロットのリンクを追加

--- app/components/OutputSlot.vue
+++ app/components/OutputSlot.vue
@@ -7,7 +7,7 @@ const {identifier, count} = parseEntry(output ?? "")
 
 <template>
   <div class="inline-flex flex-row-reverse items-end-safe">
-    <ItemImage :item="identifier" :show-link="false" class="block"/>
-    <div v-if="count !== 1" class="absolute p-0.5 m-1 bg-gray-900 backdrop-opacity-20 rounded">{{ count }}</div>
+    <ItemImage :item="identifier" class="block"/>
+    <div v-if="count !== 1" class="absolute p-0.5 m-1 bg-gray-900 backdrop-opacity-20 rounded pointer-events-none">{{ count }}</div>
   </div>
 </template>
