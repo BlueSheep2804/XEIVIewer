48225ebd463a1c208d181a888eecb93eafbad1c1
BlueSheep <48321445+BlueSheep2804@users.noreply.github.com>
2026-02-25 15:20:18 +0900

    feat: 🎸 空文字がアイテムに入った時はなにも表示しないようにした

--- app/components/ItemImage.vue
+++ app/components/ItemImage.vue
@@ -3,13 +3,15 @@ const { item: identifier, showLink = true } = defineProps({
   item: Identifier,
   showLink: Boolean
 })
+const isNone = identifier?.path === ""
 const imageUrl = `/assets/items/${identifier?.namespace}/${identifier?.path}.png`
 const linkUrl = `/item/${identifier?.full}`
 </script>
 
 <template>
-  <NuxtLink v-if="showLink" :to="linkUrl" class="inline-block">
+  <NuxtLink v-if="showLink && !isNone" :to="linkUrl" class="inline-block">
     <img :src="imageUrl" class="border-2 border-gray-700 bg-gray-400">
   </NuxtLink>
-  <img v-if="!showLink" :src="imageUrl" class="border-2 border-gray-700 bg-gray-400">
+  <img v-else-if="!showLink && !isNone" :src="imageUrl" class="inline-block border-2 border-gray-700 bg-gray-400">
+  <div v-else class="inline-block w-17 h-17 border-2 border-gray-700 bg-gray-400"></div>
 </template>
