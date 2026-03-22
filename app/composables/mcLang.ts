3d3bd91d215e34c3a9784c814fae67d6c8657edf
BlueSheep <48321445+BlueSheep2804@users.noreply.github.com>
2026-03-22 19:16:35 +0900

    feat: 🎸 ゲーム内言語の保存先をcookieに変更

--- app/composables/mcLang.ts
+++ app/composables/mcLang.ts
@@ -5,7 +5,13 @@ export const useMCLang = () => ({
   }>('mcLang')
 })
 
-export const useMCLangCode = () => useState<string>('mcLangCode', () => 'en_us')
+export const useMCLangCode = () => useCookie<string>('mc_lang_code', {
+  default() {
+    return 'en_us'
+  },
+  path: '/',
+  maxAge: 34560000
+})
 
 async function reloadLanguage() {
   useMCLang().mcLang.value = await $fetch(`/assets/lang/${useMCLangCode().value}.json`)
