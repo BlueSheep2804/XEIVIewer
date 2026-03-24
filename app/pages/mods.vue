2d7a45bbaba00e21fa59df7b7328adb5aa608b79
BlueSheep <48321445+BlueSheep2804@users.noreply.github.com>
2026-03-24 19:22:27 +0900

    feat: 🎸 mod一覧にページネーションと検索機能を追加

--- app/pages/mods.vue
+++ app/pages/mods.vue
@@ -1,22 +1,62 @@
 <script setup lang="ts">
 import type { Mod } from '~~/shared/tableTypes'
 
+const route = useRoute()
+
 const { data: mods, execute } = await useMods()
 await execute()
 
 const allMods: ComputedRef<Mod[]> = computed(() => {
   if (!Array.isArray(mods.value)) return []
-  return mods.value
+  return mods.value.filter((value) => {
+    return (
+      value.modId?.includes(search.value.mod_id ?? '')
+      && value.authors?.toLowerCase().includes(search.value.authors?.toLowerCase() ?? '')
+      && value.license?.toLowerCase().includes(search.value.license?.toLowerCase() ?? '')
+    )
+  })
 })
+const displayedMods = computed(() => {
+  return allMods.value?.slice(
+    (page.value - 1) * itemsPerPage.value,
+    page.value * itemsPerPage.value
+  )
+})
+
+const search: Ref<ModSearch> = ref({})
+const searchEntries: ComputedRef<ModSearchDefine> = computed(() => ({
+  mod_id: {
+    label: $t('mods.search.mod_id'),
+    items: []
+  },
+  authors: {
+    label: $t('mods.search.authors'),
+    items: []
+  },
+  license: {
+    label: $t('mods.search.license'),
+    items: []
+  }
+}))
+
+const page = ref(Number.parseInt(route.query?.page?.toString() ?? '1'))
+const itemsPerPage = useItemsPerPage('mods', 10)
+const total = computed(() => allMods.value?.length ?? 0)
 </script>
 
 <template>
-  <UPageSection>
+  <DatabaseView
+    v-model:search="search"
+    v-model:page="page"
+    v-model:items-per-page="itemsPerPage"
+    :entries="searchEntries"
+    :total="total"
+    :items-per-page-step="1"
+  >
     <div class="grid grid-cols-1 sm:grid-cols-2 gap-4">
-      <template v-for="mod in allMods" :key="mod.modId">
+      <template v-for="mod in displayedMods" :key="mod.modId">
         <UCard>
           <template #header>
-            <!-- <UTooltip :text="mod.fileName"> -->
             <p class="text-lg">
               {{ mod.displayName }}
             </p>
@@ -28,24 +68,14 @@ const allMods: ComputedRef<Mod[]> = computed(() => {
                 {{ mod.version }}
               </p>
             </div>
-            <!-- </UTooltip> -->
           </template>
-          <!-- <ul class="pl-4 list-disc"> -->
           <BadgeComponent icon="lucide:users" :label="$t('mods.authors')" :text="mod.authors" color="success" />
-          <!-- <p>
-            <UBadge :label="$t('mods.authors')" color="success" variant="subtle" class="mr-1.5 rounded-full" />
-            <span>{{ mod.authors }}</span>
-          </p> -->
           <BadgeComponent icon="lucide:book-open" :label="$t('mods.license')" :text="mod.license" color="warning" />
-          <!-- <p>
-            <UBadge :label="$t('mods.license')" color="warning" variant="subtle" class="mr-1.5 rounded-b-full" />
-            <span>{{ mod.license }}</span>
-          </p> -->
           <p class="mt-2">
             {{ mod.description }}
           </p>
         </UCard>
       </template>
     </div>
-  </UPageSection>
+  </DatabaseView>
 </template>
--- app/utils/searchType.ts
+++ app/utils/searchType.ts
@@ -1,5 +1,9 @@
 // 実際に受け渡しする検索データ型
 export interface Search {
+  [key: string]: string | undefined
+}
+
+export interface CommonSearch extends Search {
   mod_id?: string
   registry_id?: string
 }
@@ -11,30 +15,47 @@ export type SearchDefineEntry = {
 }
 
 export interface SearchDefine {
+  [key: string]: SearchDefineEntry
+}
+
+export interface CommonSearchDefine extends SearchDefine {
   mod_id: SearchDefineEntry
   registry_id: SearchDefineEntry
 }
 
 // アイテム
-export type ItemSearch = Search
-export type ItemSearchDefine = SearchDefine
+export type ItemSearch = CommonSearch
+export type ItemSearchDefine = CommonSearchDefine
 
 // レシピ
-export interface RecipeSearch extends Search {
+export interface RecipeSearch extends CommonSearch {
   input_id?: string
   output_id?: string
 }
 
-export interface RecipeSearchDefine extends SearchDefine {
+export interface RecipeSearchDefine extends CommonSearchDefine {
   input_id: SearchDefineEntry
   output_id: SearchDefineEntry
 }
 
 // タグ
-export interface TagSearch extends Search {
+export interface TagSearch extends CommonSearch {
   include_id?: string
 }
 
-export interface TagSearchDefine extends SearchDefine {
+export interface TagSearchDefine extends CommonSearchDefine {
   include_id: SearchDefineEntry
+}
+
+// Mod
+export interface ModSearch extends Search {
+  mod_id?: string
+  authors?: string
+  license?: string
+}
+
+export interface ModSearchDefine extends SearchDefine {
+  mod_id: SearchDefineEntry
+  authors: SearchDefineEntry
+  license: SearchDefineEntry
 }
