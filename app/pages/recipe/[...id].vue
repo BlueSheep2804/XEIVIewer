d6175b7067a8153fef1464bef29ed460c88f240a
BlueSheep <48321445+BlueSheep2804@users.noreply.github.com>
2026-05-09 00:22:11 +0900

    fix: 🐛 レシピ画面からレシピIDを一時的に削除

--- app/pages/recipe/[...id].vue
+++ app/pages/recipe/[...id].vue
@@ -1,6 +1,6 @@
 <script setup>
 const route = useRoute()
-const resourceLocation = ref(Identifier.parse(route.params.id.join('/')))
+// const resourceLocation = ref(Identifier.parse(route.params.id.join('/')))
 
 const dataUrl = `/api/recipe/${route.params.id.join('/')}`
 const { data: recipeData } = await useFetch(() => dataUrl, {
@@ -10,14 +10,9 @@ const recipe = computed(() => recipeData.value.recipes)
 </script>
 
 <template>
-  <div>
-    <UPageSection v-if="recipeData">
-      <h1 class="text-4xl font-bold">
-        {{ resourceLocation.full }}
-      </h1>
-      <div class="flex justify-center">
-        <RecipeView :recipe="recipe" />
-      </div>
-    </UPageSection>
-  </div>
+  <UPageSection v-if="recipeData">
+    <div class="flex justify-center">
+      <RecipeView :recipe="recipe" />
+    </div>
+  </UPageSection>
 </template>
